7a959d69d8bec3513646341dee80de5574660736
PHANTOM3114 <131905871+PHANTOM3114@users.noreply.github.com>
2025-06-06 00:07:07 +0200

    Server start checking at main.cc

--- src/server.cc
+++ src/server.cc
@@ -4,12 +4,23 @@ int main()
 {
     ProcessingImplemantation service;
     grpc::ServerBuilder builder;
+
+    // Set the server address and port
+
     builder.AddListeningPort("0.0.0.0:50051", grpc::InsecureServerCredentials());
     builder.RegisterService(&service);
 
+    // Start the server
+
     std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
 
-    server->Wait();
-    
+    if (!server) {
+        std::cerr << "Failed to start server" << std::endl;
+        return -1;
+    }
+    else {
+        std::cout << "Server started on port 50051" << std::endl;
+        server->Wait();
+    }
     return 0;
 }
